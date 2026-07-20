53aba6d7b54371ae19ef3e5fa38d2697b7d8e90e
Isaackchoi <isaackchoi@gmail.com>
2026-07-20 11:14:57 -0400

    feat: configure environment variables for RDS database connection

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ app = FastAPI()
 
 # 💡 從環境變數中讀取資料庫連線資訊
 DB_HOST = os.getenv("DB_HOST")
+if DB_HOST and ":" in DB_HOST:
+    DB_HOST = DB_HOST.split(":")[0]
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
